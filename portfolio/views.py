f8f931eac05870e474ba0cbfc4f0f3f65d738bb4
Crucian_Alod <k.viaznikov@yandex.ru>
2022-10-17 12:55:02 +0300

    Create apps blog, portfolio. And create models in this apps and tamplate's path in this apps

---
+++ portfolio/views.py
@@ -0,0 +1,8 @@
+from urllib import request
+from django.shortcuts import render
+from .models import Project
+
+
+def home(request):
+    projects = Project.objects.all()
+    return render(request, 'portfolio/home.html', {'projects': projects})
